dc03434abebe143aeeeb7d25a474303bca8415dd
ErineJassen <erinejassen@gmail.com>
2024-03-05 01:10:16 +0800

    sdsa

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btn = (Button)findViewById(R.id.signup);
+        TextView btn = (TextView)findViewById(R.id.signup);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Button btn = (Button)findViewById(R.id.login);
+        TextView btn = (TextView) findViewById(R.id.login);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
